ba653ce95c11ab42803dd7d5b4bef6857b9b0a5b
a_lampcat <fatkulin@a2design.biz>
2015-01-05 20:40:39 +0600

    Added temp db parsing

--- avto-lux/app/configparser.py
+++ avto-lux/app/configparser.py
@@ -3,8 +3,6 @@ import os, sys
 
 
 class Config:
-	ENV_VARS = ('PORT', 'HOST')
-
 	def __init__(self):
 		conf_path = os.path.join(os.getcwd(), 'config.yaml')
 		if os.environ.get('CONFIG_PATH'):
@@ -14,9 +12,21 @@ class Config:
 		config = yaml.load(''.join(fd.readlines()))
 		fd.close()
 
-		for key in self.ENV_VARS:
-			if os.environ.get(key):
-				config[key] = os.environ.get(key)
+		if os.environ.get('PORT'):
+			config['PORT'] = os.environ.get('PORT')
+		if os.environ.get('HOST'):
+			config['HOST'] = os.environ.get('HOST')
+		if os.environ.get('DATABASE_PORT'):
+			config['DATABASE']['PORT'] = os.environ.get('DATABASE_PORT')
+		if os.environ.get('DATABASE_HOST'):
+			config['DATABASE']['HOST'] = os.environ.get('DATABASE_HOST')
+		if os.environ.get('DATABASE_DBNAME'):
+			config['DATABASE']['DBNAME'] = os.environ.get('DATABASE_DBNAME')
+		if os.environ.get('DATABASE_USER'):
+			config['DATABASE']['USER'] = os.environ.get('DATABASE_USER')
+		if os.environ.get('DATABASE_PASS'):
+			config['DATABASE']['PASS'] = os.environ.get('DATABASE_PASS')
+
 		self.config = config
 
 	def __call__(self, key):
