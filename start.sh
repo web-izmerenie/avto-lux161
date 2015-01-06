be87d82100a7b132ff767477c34337ddfe746c2a
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2015-01-06 16:28:44 +0300

    added missed quotes for start.sh

--- start.sh
+++ start.sh
@@ -2,11 +2,11 @@
 
 cd "`dirname "$0"`"
 
-[ -z "$PYTHON3_BIN" ] && PYTHON3_BIN=$(which python3 2>/dev/null)
+[ -z "$PYTHON3_BIN" ] && PYTHON3_BIN="$(which python3 2>/dev/null)"
 
 if [ ! -x "$PYTHON3_BIN" ]; then
 	echo 'python3 not found.' >&2
 	exit 1
 fi
 
-$PYTHON3_BIN avto-lux/main.py
+"$PYTHON3_BIN" avto-lux/main.py
