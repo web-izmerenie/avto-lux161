acb655367e6ba593c7f2b025ad45488bf28bdb44
AndrwFn <fn.andrw@gmail.com>
2015-01-11 21:54:47 +0600

    Added forms validation

--- avto-lux/app/core/routes/main.py
+++ avto-lux/app/core/routes/main.py
@@ -1,6 +1,8 @@
 import json
 import tornado.template
+from tornado.web import HTTPError, MissingArgumentError
 from .base import BaseHandler
+
 from app.mixins.routes_mixin import Custom404Mixin, JsonResponseMixin
 from pyjade.ext.tornado import patch_tornado
 
@@ -17,7 +19,14 @@ from app.models.catalogmodels import(
 	CatalogSectionModel,
 	CatalogItemModel
 )
+
+from app.models.utilmodels import (
+	CallModel,
+	OrderModel
+)
 from sqlalchemy.orm.exc import MultipleResultsFound, NoResultFound
+import time
+import datetime
 
 patch_tornado()
 
@@ -45,7 +54,89 @@ class ItemRoute(BaseHandler, Custom404Mixin):
 	def get(self, category, item):
 		return self.render('yyy.jade') #Replace to catalog.jade
 
-class FormHandler(BaseHandler, JsonResponseMixin):
+
+class FormsHandler(JsonResponseMixin):
 	def post(self):
-		action = request.body
-		return json_response({})
+		actions = {
+			'call' : {
+				'fn': self.save_call,
+			},
+			'order' : {
+				'fn': self.save_order,
+			}
+		}
+		is_ajax = None
+		try:
+			is_ajax = self.get_argument('json')
+		except MissingArgumentError:
+			pass
+
+		body = str(self.request.body)
+		action = self.get_argument('action')
+
+		if action not in actions.keys():
+			if is_ajax:
+				self.set_status(500)
+				return self.json_response({'status': 'systemfail'})
+			return self.write("Lol, request isn't correct")
+
+		fn = actions[action]['fn']
+		args = dict([ x.split('=') for x in body.split('&') if 'action' not in x ])
+
+		if self.validate_fields(args):
+			try:
+				fn(args)
+			except Exception as e:
+				print(e)
+				self.set_status(500)
+				return self.json_response({'status': 'systemfail'})\
+					if is_ajax\
+					else self.write('Here should be a 500 page')
+
+			if is_ajax:
+				return self.json_response({'status': 'success'})
+			self.write('Lol, it is not ajax. Loosers.')
+
+
+	def validate_fields(self, fields):
+		print(fields)
+		required_fields = ['name', 'phone', '']
+		err_stack = []
+
+		for key in fields:
+			if key in required_fields and fields[key] is '':
+				err_stack.append(key)
+
+		if len(err_stack) > 0:
+			self.json_response({
+				'status': 'error',
+				'errorlist': [{x: 'reuqired' for x in err_stack}]
+				})
+			return False
+		return True
+
+
+	def save_call(self, d):
+		call = CallModel(
+			name = d['name'],
+			phone = d['phone'],
+			date = datetime.datetime.now()
+			)
+		session.add(call)
+		session.commit()
+
+
+	def save_order(self, d):
+		car = session.query(CatalogItemModel).filter_by(id=d['id'])
+		order = OrderModel(
+			name=d['name'],
+			email=d['email'],
+			phone=d['phone'],
+			# date = datetime.combine(call_date, call_time)
+			date=datetime.datetime.now()
+			)
+		car.orders.append(order)
+
+		session.add(car)
+		session.add(order)
+		session.commit()
