03143be3eba4049d730f62865d626147d5fe7c00
AndrwFn <fn.andrw@gmail.com>
2015-01-12 23:24:40 +0600

    Added devserver

--- avto-lux/manage.py
+++ avto-lux/manage.py
@@ -4,9 +4,13 @@ from app.configparser import config
 from app.app import run_instance
 from app.models.init_models import init_models
 
+devserver = config('DEVSERVER')
 
-def run(port=0, host=0):
-	run_instance(config('PORT'), host=config('HOST'))
+def error_handler(fn):
+	pass
+
+def devserver(port=devserver['PORT'], host=devserver['HOST']):
+	run_instance(port=port, host=host)
 
 
 def dbsync():
@@ -17,26 +21,36 @@ def dbsync():
 		print(e)
 
 
-actions = {
-	'run': run,
-	'dbsync': dbsync,
-}
-
 
 if __name__ == '__main__':
 	try:
 		action = sys.argv[1]
-		opts = sys.argv[2:]
-		if len(opts) > 0:
-			try:
-				opts, args = getopt.getopt(sys.argv[2:], "ho:v", ["help", "output="])
-			except Exception as e:
-				raise Exception("Unknown option key")
-		print(opts)
+		# opts = sys.argv[2:]
+		# if len(opts) > 0:
+		# 	try:
+		# 		opts, args = getopt.getopt(sys.argv[2:], "p", ["help", "output="])
+		# 	except Exception as e:
+		# 		raise Exception("Unknown option key")
+		# print(opts)
 
 	except IndexError:
 		raise Exception("Missed required argument")
+
+	actions = {
+		'devserver': {
+			'fn': devserver,
+			'kwrgs': {},
+			'options': []
+		},
+		'dbsync': {
+			'fn': dbsync,
+			'kwrgs': {},
+			'options': []
+		},
+	}
+
 	if action not in actions:
 		raise Exception("Invalid option")
 
-	actions[action]()
+	ac = actions[action]
+	ac['fn'](**ac['kwrgs'])
